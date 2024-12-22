e06ebdd96abcfaf6d359c809daf35a0649eee8c8
KevinVerbruggen1 <78963576+KevinVerbruggen1@users.noreply.github.com>
2024-12-22 01:04:31 +0100

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {StockOverviewComponent} from "./components/stock-management/stock-overview/stock-overview.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'stock-overview',
+    component: StockOverviewComponent
+  },
+
+];
